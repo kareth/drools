26e72187ec22450b7761a753d511568849194a86
Piotr Żurkowski <kareth92@gmail.com>
2013-11-27 17:00:29 +0100

    init

---
+++ src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,96 @@
+package com.sample;
+
+import org.drools.KnowledgeBase;
+import org.drools.KnowledgeBaseFactory;
+import org.drools.builder.KnowledgeBuilder;
+import org.drools.builder.KnowledgeBuilderError;
+import org.drools.builder.KnowledgeBuilderErrors;
+import org.drools.builder.KnowledgeBuilderFactory;
+import org.drools.builder.ResourceType;
+import org.drools.io.ResourceFactory;
+import org.drools.logger.KnowledgeRuntimeLogger;
+import org.drools.logger.KnowledgeRuntimeLoggerFactory;
+import org.drools.runtime.StatefulKnowledgeSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+            KnowledgeBase kbase = readKnowledgeBase();
+            StatefulKnowledgeSession ksession = kbase.newStatefulKnowledgeSession();
+            KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory.newFileLogger(ksession, "test");
+            // go !
+            //
+            ksession.fireAllRules();
+            ksession.insert(new Firefighters());
+            ksession.fireAllRules();
+            logger.close();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    private static KnowledgeBase readKnowledgeBase() throws Exception {
+        KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
+        kbuilder.add(ResourceFactory.newClassPathResource("Sample.drl"), ResourceType.DRL);
+        KnowledgeBuilderErrors errors = kbuilder.getErrors();
+        if (errors.size() > 0) {
+            for (KnowledgeBuilderError error: errors) {
+                System.err.println(error);
+            }
+            throw new IllegalArgumentException("Could not parse knowledge.");
+        }
+        KnowledgeBase kbase = KnowledgeBaseFactory.newKnowledgeBase();
+        kbase.addKnowledgePackages(kbuilder.getKnowledgePackages());
+        return kbase;
+    }
+
+    public static class Feedback {
+        private boolean smoke;
+        private double temperature;
+
+        public boolean getSmoke() {
+            return this.smoke;
+        }
+
+        public void setSmoke(boolean smoke) {
+            this.smoke = smoke;
+        }
+
+		public double getTemperature() {
+			return temperature;
+		}
+
+		public void setTemperature(double temperature) {
+			this.temperature = temperature;
+		}
+    }
+    
+    public static class Fire {}
+    
+    public static class Phone {
+    	public Phone(String number) {
+			super();
+			this.number = number;
+		}
+
+		private String number;
+
+		public String getNumber() {
+			return number;
+		}
+
+		public void setNumber(String number) {
+			this.number = number;
+		}
+    	
+    }	
+    
+    public static class Firefighters {	
+    }
+
+}
